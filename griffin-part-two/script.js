0b7344a4632e3209d63183399e549a79e71d923a
kaleighgriffin <77368035+kaleighgriffin@users.noreply.github.com>
2021-03-13 18:50:00 -0500

    Add files via upload

---
+++ griffin-part-two/script.js
@@ -0,0 +1,11 @@
+let name = prompt("What is your pet's name?");
+console.log(name);
+
+let answer = prompt("Did you feed, pet or walk him/her?");
+console.log(answer);
+
+for (let i = 0; i < 5; i++) {
+    console.log('answer' + i);
+    if(i === 0) break;
+    if(answer === feed )
+}
